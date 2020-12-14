4e05eda83ec7599956d2413fa2d4df545ab802fe
Kostia Netot <pugjazz@gmail.com>
2020-12-14 13:12:52 +0200

    add comments to post and like to post implementd

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-const { UserInputError } = require('apollo-server');
+const { UserInputError, AuthenticationError } = require('apollo-server');
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
@@ -19,14 +19,57 @@ module.exports = {
       const post = await Post.findById(postId);
 
       if(post) {
-        post.comments.unsift({
+        post.comments.unshift({
           body,
           username,
           createdAt: new Date().toISOString()  
         })
         await post.save();
         return post; 
+      } else throw new UserInputError('Post not found');
+    }, 
+
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post; 
+        } else {
+          throw new AuthenticationError('Action not allowed');
+        }
+      } else {
+        throw new UserInputError('Post not found');
       }
+    },
+
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if(post) {
+
+        if(post.likes.find(like => like.username === username)) {
+          // Post already likes, unlike it
+          post.likes = post.likes.filter(like => like.username !== username);          
+        } else {
+          // Not liked, like post
+          post.likes.push({
+            username, 
+            createdAt: new Date.toISOString()
+          });
+        }
+
+        await post.save();
+        return post;
+      } else throw new UserInputError('Post not found');
     }
   }
-}
+};
