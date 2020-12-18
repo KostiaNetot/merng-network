b3823e36ce371cca1dbe7743090ee8fe16151adb
KostiaNetot <pugjazz@gmail.com>
2020-12-18 10:07:53 +0200

    done with server part

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 const Post = require('./../../models/Post');
 const checkAuth = require('./../../util/check-auth');
@@ -38,10 +38,15 @@ module.exports = {
         createdAt: new Date().toISOString()
       });
 
-      const post = await newPost.save()
+      const post = await newPost.save();
+
+      context.pubsub.publish('NEW_POST', {
+        newPost: post
+      });
 
       return post;
     },
+
     async deletePost(_, { postId }, context) {
       const user = checkAuth(context);
 
@@ -56,6 +61,33 @@ module.exports = {
       } catch(err) {
         throw new Error(err);       
       }
+    },
+
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if(post) {
+
+        if(post.likes.find(like => like.username === username)) {
+          // Post already likes, unlike it
+          post.likes = post.likes.filter(like => like.username !== username);        
+        } else {
+          // Not liked, like post
+          post.likes.push({
+            username, 
+            createdAt: new Date().toISOString()
+          });
+        }
+        await post.save();
+        return post;
+      } else throw new UserInputError('Post not found');
+    }
+  },
+  Subscribtion: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
     }
   }
 };
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config');
 
+const pubsub = new PubSub();
+
 const PORT = 5000;
 
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
-	context: ({ req }) => ({ req })
+	context: ({ req }) => ({ req, pubsub })
 });
 
 mongoose.connect(MONGODB, { useNewUrlParser: true })
