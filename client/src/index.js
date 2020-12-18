f9895bd1c422ba6825d8bb90b70442da1cb25d0d
KostiaNetot <pugjazz@gmail.com>
2020-12-18 13:14:57 +0200

    client routing navbar

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ApolloProvider from './apollo-provider/index';
+import ApolloProvider from './apollo-provider';
 
 ReactDOM.render(
-    <App />,
+  ApolloProvider,
   document.getElementById('root')
 );
