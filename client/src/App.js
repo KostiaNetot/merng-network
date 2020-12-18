955c376b6a0180489050de4be64fc5272d948baa
KostiaNetot <pugjazz@gmail.com>
2020-12-18 11:11:41 +0200

    apollo provider setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Appppp
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import ApolloProvider from './apollo-provider/index';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
